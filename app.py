33618155a343c0b7ae1a885f0d748412fc628d27
jinwonro <65985868+jinwonro@users.noreply.github.com>
2020-12-12 17:05:19 +0900

    my project_jin

--- app.py
+++ app.py
@@ -1,1 +1,47 @@
-print("hello")
+from selenium import webdriver
+from datetime import date, timedelta
+import time
+import pyautogui
+import requests
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome('C:\Program Files\Google\Chrome\Application\chromedriver.exe')
+
+driver.get('https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=코로나')
+
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="search_option_button"]').click()
+
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="_nx_search_option_date_link"]').click()
+
+# 매월, 매초 구하기
+today = date.today()
+first_day = today.replace(day=1)
+
+last_day_month_ago = first_day - timedelta(days=1)
+first_day_month_ago = last_day_month_ago.replace(day=1)
+
+time.sleep(1)
+driver.find_element_by_id("_nx_date_from").click()
+
+time.sleep(1)
+pyautogui.write(first_day_month_ago.strftime('%Y%m%d'), interval=0.5)
+
+time.sleep(1)
+driver.find_element_by_id("_nx_date_to").click()
+time.sleep(1)
+pyautogui.write(last_day_month_ago.strftime('%Y%m%d'), interval=0.5)
+
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="snb"]/div/ul/li[2]/div/div[1]/span/button').click()
+
+raw = requests.get('https://search.naver.com/search.naver?&where=news&query=코로나', headers={'User-Agent': 'Mozilla/5.0'}).text
+html = BeautifulSoup(raw, 'html.parser')
+
+articles = html.select('.list_news > li')
+
+for article in articles:
+    title = article.select_one('a.news_tit').text
+
+    print(title)
--- main.py
+++ main.py
@@ -1,16 +1,72 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+## API 역할을 하는 부분
+@app.route('/search', methods=['GET'])
+def write_searching():
+    # query_receive로 클라이언트가 준 query 가져오기
+    query_receive = request.args.get('query_give')
+    # start_day_receive로 클라이언트가 준 start_day 가져오기
+    start_day_receive = request.args.get('start_day_give')
+    #  last_day_receive로 클라이언트가 준  last_day 가져오기
+    last_day_receive = request.args.get('last_day_give')
 
-# Press the green button in the gutter to run the script.
+# html 파트에서 진행 가능하게 작업
+#query = input('검색할 키워드 : ')
+#start_day = input('시작날짜 : ')
+#last_day = input('종료날짜 : ')
+
+    filename = "company_data.xlsx" #파일명
+    book = openpyxl.load_workbook(filename) #엑셀파일 book 변수에 저장
+    sheet = book.worksheets[0]
+
+    datas = []
+    for row in sheet.rows:
+        datas.append(
+            row[0].value
+        )
+
+        title_count = 0
+        for page in range(10):
+            raw = requests.get('https://search.naver.com/search.naver?&where=news&query=' + query_receive + '&sm=tab_pge&sort=0&photo=0&field=0&reporter_article=&pd=4&ds=' + start_day_receive + '&de=' + last_day_receive +'&docid=&nso=so:r,p:1d,a:all&mynews=0&cluster_rank=87&start=' + str(page+1) + '&refresh_start=0', headers={'User-Agent': 'Mozilla/5.0'}).text
+            html = BeautifulSoup(raw, 'html.parser')
+
+            articles = html.select('.list_news > li')
+
+            for data in datas:
+                for article in articles:
+                    title = article.select_one('a.news_tit').text
+                    title_count += title.count(data[0])
+                    # list를 만들어내라
+                    # DB에 삽입할 review 만들기
+                    title_count = {
+                      'count': title_count,
+                    }
+
+                    db.title_count.insert_one(title_count)
+                    title_count = list(db.title_count.find({}, {'_id': 0}))
+                    return jsonify({'result': 'success', 'title_count': title_count})
+
+        #        print(title)
+
+    #    print(title_count)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run('0.0.0.0', port=5000, debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 'https://search.naver.com/search.naver?&where=news&query=' + query + '&sm=tab_tmr&frm=mr&nso=so:r,p:from' + start_day + 'to' + last_day +',a:all&sort=0'
+# 'https://search.naver.com/search.naver?sm=tab_opt&where=nexearch&query='+query+'&oquery='+ query +'&tqi=U9M6vdprvTVssa1zHeCssssstzN-208450&nso=so%3Ar%2Cp%3Afrom'+ start_day + 'to' + last_day + '%2Ca%3Aall'
+
+# 'https://search.naver.com/search.naver?&where=news&query=' + query + '&sm=tab_pge&sort=0&photo=0&field=0&reporter_article=&pd=4&ds=' + start_day + '&de=' + last_day +'&docid=&nso=so:r,p:1d,a:all&mynews=0&cluster_rank=87&start=61&refresh_start=0'
